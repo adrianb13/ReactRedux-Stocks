8f746a051d3667a6a18c686b379f12fa83eda242
Adrian <coding4adrian@yahoo.com>
2021-02-17 17:35:28 -0800

    stock page update

--- client/src/components/Stocks/index.js
+++ client/src/components/Stocks/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
 //import { connect } from "react-redux";
+import "./stocks.css";
 
 class Stocks extends React.Component {
   state = {
@@ -9,8 +10,55 @@ class Stocks extends React.Component {
 
   render () {
     return (
-      <div>
-        <div>Hello</div>
+      <div className="backBlack">
+        <div className="sHeaderBox">
+          <div className="sHeader">TICKER ME THIS</div>
+        </div>
+        <div className="sDBArea">
+          <div className="sDotBorder"></div>
+          <div className="sHeaderText"><span className="sRed">DON'T DELAY,</span> INVEST IN YOUR FUTURE NOW! <span style={{marginLeft: "30px"}}>UR {"\u25B2"} 100%</span></div>
+          <div className="sDotBorder"></div>
+        </div>
+        <br />
+        <div className="sSearchBox">
+          <div className="sSearch">Search</div>
+          <input className="sSearchInput" type="text" placeholder="Name or Ticker Symbol"></input>
+        </div>
+
+        <div className="sTableArea">
+          <table className="sTable">
+            <thead>
+              <tr>
+                <th>Symbol</th>
+                <th style={{width: "250px"}}>Name</th>
+                <th>Current</th>
+                <th>+/- ($)</th>
+                <th>% Change</th>
+                <th>Open</th>
+                <th>Today's High</th>
+                <th>Today's Low</th>
+                <th>52-Wk High</th>
+                <th>52-Wk Low</th>
+                <th>Volume</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>XYZ</td>
+                <td>XYZ Inc.</td>
+                <td>$5.00</td>
+                <td>+1.00</td>
+                <td>+25.0%</td>
+                <td>$4.00</td>
+                <td>$5.25</td>
+                <td>$3.90</td>
+                <td>$5.25</td>
+                <td>$2.00</td>
+                <td>10M</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     )
   }
--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import App from "./App";
 //import reportWebVitals from "./reportWebVitals";
+import "./index.css";
 
 import store from "./store/index";
 
