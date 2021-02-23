08e60e450b117389568c76e4b85b9c0739e2b4ee
Adrian <coding4adrian@yahoo.com>
2021-02-23 00:28:07 -0800

    configure search API

--- client/src/components/Stocks/index.js
+++ client/src/components/Stocks/index.js
@@ -1,11 +1,120 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
 //import { connect } from "react-redux";
+import API from "../../utils/API";
 import "./stocks.css";
+
 
 class Stocks extends React.Component {
   state = {
+    query: null,
+    bestMatches: null,
+    ticker: null,
+    tickerName: null,
+    currentStockInfo: null,
+    currentStockQuote: null,
+    searchBox: false,
+    stockAvailable: false
+  }
 
+  handleInputChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    })
+  }
+
+  enterSubmit = (event) => {
+    if(event.key === "Enter"){
+      this.findSymbol(event);
+    }
+  }
+
+  findSymbol = (event) => {
+    event.preventDefault();
+    this.setState({
+      stockAvailable: false,
+      query: ""
+    })
+    
+    let search = this.state.query
+    API.findStockSymbol(search)
+      .then(res => {
+        console.log(res.data.tickers)
+        let results = res.data.tickers;
+        
+        if(results.length > 1){
+          let bestMatches = results.filter(stock => stock.currency === "USD")
+          console.log(bestMatches)
+          
+          if(bestMatches.length === 1){
+            let current = bestMatches[0];
+            this.quoteSymbol(current);
+          } else {
+            this.setState({
+              bestMatches: bestMatches
+            })
+          }
+
+        } else if (results.length === 1) {
+          let current = results[0]
+          this.quoteSymbol(current);
+        } else {
+          console.log("no matches")
+        }
+        
+/*         let tickerSymbol = res.data.result[0].displaySymbol;
+        let currentStockInfo = res.data.result[0]
+        if(tickerSymbol){
+          this.setState({
+            currentStockInfo: currentStockInfo
+          }) */
+/*           API.quoteSymbol(tickerSymbol)
+            .then(res => {
+              console.log(res.data)
+              let change = res.data.c - res.data.pc;
+              let percent = change / res.data.pc * 100;
+              this.setState({
+                currentStockQuote: res.data,
+                currentStockChange: change,
+                currentStockPercent: percent,
+                stockAvailable: true,
+              })
+            }) 
+        } */
+      })
+      .catch(err => console.log(err))
+  }
+
+  quoteSymbol = (symbol) => {
+    let currentSymbol = symbol.ticker;
+    let currentName = symbol.name;
+    this.setState({
+      ticker: currentSymbol,
+      tickerName: currentName
+    })
+
+    API.quoteSymbol(currentSymbol)
+      .then(res => {
+        console.log(res.data)
+        let quote = res.data["Global Quote"]
+        this.setState({
+          currentStockQuote: quote,
+          stockAvailable: true
+        })
+
+        //Finnhub Quote - Must update UI states below
+        /* let change = res.data.c - res.data.pc;
+        let percent = change / res.data.pc * 100;
+        change = change.toFixed(2);
+        percent = percent.toFixed(2);
+        this.setState({
+          currentStockQuote: res.data,
+          currentStockChange: change,
+          currentStockPercent: percent,
+          stockAvailable: true,
+        }) */
+      })
   }
 
   render () {
@@ -20,45 +129,56 @@ class Stocks extends React.Component {
           <div className="sDotBorder"></div>
         </div>
         <br />
-        <div className="sSearchBox">
-          <div className="sSearch">Search</div>
-          <input className="sSearchInput" type="text" placeholder="Name or Ticker Symbol"></input>
+        <div className="sSearchBox" onChange={this.handleInputChange}>
+          <button className="sSearch" type="submit" onClick={this.findSymbol}>Search</button>
+          <input className="sSearchInput" type="text" name="query" id="query" onKeyDown={this.enterSubmit} placeholder="Name or Ticker Symbol"></input>
         </div>
 
-        <div className="sTableArea">
-          <table className="sTable">
-            <thead>
-              <tr>
-                <th>Symbol</th>
-                <th style={{width: "250px"}}>Name</th>
-                <th>Current</th>
-                <th>+/- ($)</th>
-                <th>% Change</th>
-                <th>Open</th>
-                <th>Today's High</th>
-                <th>Today's Low</th>
-                <th>52-Wk High</th>
-                <th>52-Wk Low</th>
-                <th>Volume</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>XYZ</td>
-                <td>XYZ Inc.</td>
-                <td>$5.00</td>
-                <td>+1.00</td>
-                <td>+25.0%</td>
-                <td>$4.00</td>
-                <td>$5.25</td>
-                <td>$3.90</td>
-                <td>$5.25</td>
-                <td>$2.00</td>
-                <td>10M</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+        {this.state.searchBox ? (
+          <div>
+            {this.state.bestMatches.map(match => (
+              <table className="sSearchTable">
+                <thead>
+
+                </thead>
+              </table>
+            ))}
+          </div>
+        ) : (null)}
+
+        {this.state.stockAvailable ? (
+          <div className="sTableArea">
+            <table className="sTable">
+              <thead>
+                <tr>
+                  <th>Symbol</th>
+                  <th style={{width: "250px"}}>Name</th>
+                  <th>Current*</th>
+                  <th>+/- ($)</th>
+                  <th>% Change</th>
+                  <th>Open</th>
+                  <th>Today's High</th>
+                  <th>Today's Low</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{this.state.ticker}</td>
+                  <td>{this.state.tickerName}</td>
+                  <td>${this.state.currentStockQuote["05. price"].slice(0, -2)}</td>
+                  <td>{this.state.currentStockQuote["09. change"].slice(0, -2)}</td>
+                  <td>{this.state.currentStockQuote["10. change percent"].slice(0, -2)}</td>
+                  <td>${this.state.currentStockQuote["02. open"].slice(0, -2)}</td>
+                  <td>${this.state.currentStockQuote["03. high"].slice(0, -2)}</td>
+                  <td>${this.state.currentStockQuote["04. low"].slice(0, -2)}</td>
+                </tr>
+              </tbody>
+            </table>
+            <div className="sPriceDisclaimer">* Current pricing does not include After-Hours price changes when Market is closed.</div>
+          </div>
+        ) : (null)}
+
+        <div></div>
       </div>
     )
   }
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,5 +1,15 @@
 import axios from "axios";
 
+const APIKEYPOLYGON = "&apiKey=" + process.env.REACT_APP_POLYGON_API_KEY;
+const BASEURLPOLYGON = process.env.REACT_APP_POLYGON_BASE;
+
+const APIKEYALPHA = "&apikey=" + process.env.REACT_APP_ALPHA_API_KEY;
+const BASEURLALPHA = process.env.REACT_APP_ALPHA_BASE;
+
+//const APIKEYFINNHUB = "&token=" + process.env.REACT_APP_FINNHUB_API_KEY;
+//const BASEURLFINNHUB = process.env.REACT_APP_FINNHUB_BASE;
+
+
 export default {
   getStocks: () => {
     return axios.get("/api/stocks")
@@ -8,10 +18,34 @@ export default {
     return axios.post("/api/stocks", stockData);
   },
   updateStock: (stock) => {
-    return axios.put("/api/stocks/" + stock.id, stock)
+    return axios.put("/api/stocks/" + stock.id, stock);
   },
   deleteStock: (stockId) => {
     return axios.delete("/api/stocks/" + stockId);
+  },
+
+  /* findStockSymbol: (query) => {
+    let search = "search?q=" + query + "&symbol?exchange=US";
+    return axios.get(BASEURLFINNHUB + "search?q=" + query + APIKEYFINNHUB);
+  },
+  */
+
+  findStockSymbol: (query) => {
+    //Alpha Search
+    //let search = "function=SYMBOL_SEARCH&keywords=" + query + APIKEYALPHA;
+    //return axios.get(BASEURLALPHA + search);
+    
+    //Polygon Search
+    let search = "tickers?sort=ticker&locale=us&search=" + query +"&perpage=10&page=1" + APIKEYPOLYGON
+    return axios.get(BASEURLPOLYGON + search)
+
+  },
+  quoteSymbol: (symbol) => {
+    //Finnhub Quote
+    //return axios.get(BASEURLFINNHUB + "quote?symbol=" + symbol + APIKEYFINNHUB);
+    
+    let quote = "function=GLOBAL_QUOTE&symbol=" + symbol + APIKEYALPHA;
+    return axios.get(BASEURLALPHA + quote);
   }
 
 }
