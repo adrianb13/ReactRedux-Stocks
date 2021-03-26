b274aa6bcfc2b9dc7cd44e2f5e3972beca17b6a4
Adrian <coding4adrian@yahoo.com>
2021-03-25 17:59:49 -0700

    update news page

--- client/src/components/MarketNews/index.js
+++ client/src/components/MarketNews/index.js
@@ -1,40 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { withRouter } from "react-router-dom";
-import { connect } from "react-redux";
-import * as actions from "../../actions/index";
-import { bindActionCreators } from "redux";
 import "./marketNews.css";
+import API from "../../utils/API";
 
 import Banner from "../Banner/index";
 
-class MarketNews extends React.Component {
-  state = {
-    marketNews: null,
-    showNews: false
-  };
+const MarketNews = () => {
+  const [marketNews, setMarketNews] = useState([]);
+  const [showNews, setShowNews] = useState(false);
+  
+  useEffect(() => {
+    fetchNews()
+  }, []);
 
-  componentDidMount = () => {
-    this.getNews();
-  };
-
-  getNews = () => {
-    this.props.actions.marketNewsInfo()
-      .then (res => {
-        let marketNews = this.props.marketNews
-        this.setState({
-          marketNews: marketNews,
-          showNews: true
-        })
+  //Gets Market News from API
+  const fetchNews = async () => {
+    API.marketNews()
+      .then(res => {
+        setMarketNews(res.data)
+        setShowNews(true)
       })
   };
 
-  dateConvert = (timestamp) => {
+  //Converts UNIX timestamp to Local Date String
+  const dateConvert = (timestamp) => {
     let date = new Date((timestamp * 1000)).toString();
     date = new Date(date).toLocaleDateString("en-US").split("-")
     return date;
-  }
+  };
 
-  newsIcon = (icon) => {
+  //Styles Article Icon
+  const newsIcon = (icon) => {
     let styling = {
       width: "100px",
       height: "70px",
@@ -43,49 +39,37 @@ class MarketNews extends React.Component {
       borderRadius: "5px"
     }
     return styling;
-  }
+  };
 
-  render () {
-    return (
-      <div className="backBlack">
-        <div>
-          <Banner />
-          <div className="sDotBorder"></div>
-          {this.state.showNews ? (
-            <div>
-              <div className="mnHeader">Latest News</div>
-              {this.state.marketNews.map(news => (
-                <div className="mnArticleBox" key={news.id}>
-                  <div style={this.newsIcon(news.image)}></div>
-                  <div className="mnArticle">
-                    <a href={news.url} target="_blank" rel="noreferrer">
-                      <div className="mnHeadline">{news.headline}</div>
-                      <div className="mnDate">Posted - {this.dateConvert(news.datetime)}</div>
-                      <div className="mnSummary">{news.summary}</div>
-                    </a>
-                  </div>
+  return (
+    <div className="backBlack">
+      <div>
+        <Banner />
+        <div className="sDotBorder"></div>
+        {showNews ? (
+          <div>
+            <div className="mnHeader">Latest News</div>
+            {marketNews.map(news => (
+              <div className="mnArticleBox" key={news.id}>
+                <div style={newsIcon(news.image)}></div>
+                <div className="mnArticle">
+                  <a href={news.url} target="_blank" rel="noreferrer">
+                    <div className="mnHeadline">{news.headline}</div>
+                    <div className="mnDate">Posted - {dateConvert(news.datetime)}</div>
+                    <div className="mnSummary">{news.summary}</div>
+                  </a>
                 </div>
-              ))}
-            </div>
-          ) : (
-            <div>
-              <div className="mnFindNewsHeader">The reporter is out searching for breaking news!!!</div>
-            </div>
-          )}
-        </div>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div>
+            <div className="mnFindNewsHeader">The reporter is out searching for breaking news!!!</div>
+          </div>
+        )}
       </div>
-    )
-  };
+    </div>
+  )
 };
 
-const mapStateToProps = (state) => {
-  return {
-    marketNews: state.marketNews
-  };
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return { actions: bindActionCreators(actions, dispatch)}
-};
-
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(MarketNews));
+export default withRouter((MarketNews));
